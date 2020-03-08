1e40cb0bbd7e4e0f71c0aa46780aec1d6f03dbd6
Joaquim Monteiro <joaquim.monteiro@protonmail.com>
2020-03-08 01:31:58 +0000

    Generate (non-exhaustive) enums instead of constants

--- build.rs
+++ build.rs
@@ -9,6 +9,14 @@ fn main() {
 
     let bindings = bindgen::Builder::default()
     .header("wrapper.h")
+    .rustified_non_exhaustive_enum("PedDeviceType")
+    .rustified_non_exhaustive_enum("PedUnit")
+    .rustified_non_exhaustive_enum("_PedDiskFlag")
+    .rustified_non_exhaustive_enum("_PedDiskTypeFeature")
+    .rustified_non_exhaustive_enum("_PedExceptionOption")
+    .rustified_non_exhaustive_enum("_PedExceptionType")
+    .rustified_non_exhaustive_enum("_PedPartitionFlag")
+    .rustified_non_exhaustive_enum("_PedPartitionType")
     .generate()
     .expect("Unable to generate bindings");
 
